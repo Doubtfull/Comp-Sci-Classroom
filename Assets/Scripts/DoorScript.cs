35abbc9bf8f93557d38d183d13512e702317eccf
Doubtfull <myjoyishere13@gmail.com>
2024-12-12 10:51:55 -0800

    Initial commit

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public GameObject Intructions;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Intructions.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                animator.SetTrigger("OpenClose");
+
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Intructions.SetActive(false);
+        }
+    }
+}
